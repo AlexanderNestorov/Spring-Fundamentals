735c28feaf1bc6b5e83437c4e5eece36c7176d3e
Alexander Nestorov <jak.racer@gmail.com>
2021-10-20 15:07:20 +0300

    Initial Commit.

---
+++ ExamPrep/ExamPrep21_02_21/ExamPrep21_02/src/main/java/com/example/examprep21_02/ExamPrep2102Application.java
@@ -0,0 +1,13 @@
+package com.example.examprep21_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPrep2102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPrep2102Application.class, args);
+    }
+
+}
